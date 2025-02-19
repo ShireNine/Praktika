f0679691f0e4a10b0bc993bcda0aa13e25b415cf
ShireNine <168739802+ShireNine@users.noreply.github.com>
2025-02-19 19:38:31 +0500

    5_File

--- Prakitka.py
+++ Prakitka.py
@@ -1,14 +1,10 @@
 import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
-cursor.execute('''
-SELECT username, age
-FROM Users
-WHERE age = (SELECT MAX(age) FROM Users)
-''')
-oldest_users = cursor.fetchall()
+cursor.execute('SELECT * FROM Users')
+users = cursor.fetchall()
 
-for user in oldest_users:
+for user in users:
     print(user)
 
 connection.close()

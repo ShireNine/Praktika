16a8eeb91636762211ab8baf9582054e17b0bb04
ShireNine <168739802+ShireNine@users.noreply.github.com>
2025-02-19 19:29:51 +0500

    4C_File

--- Prakitka.py
+++ Prakitka.py
@@ -1,15 +1,38 @@
+##import sqlite3
+##connection = sqlite3.connect('my_database.db')
+##cursor = connection.cursor()
+##cursor.execute('SELECT SUM (age) FROM Users')
+##total_users = cursor.fetchone()[0]
+##
+##print('Общее количество пользоватлей', total_users)
+##connection.close()
+
+
+##import sqlite3
+##connection = sqlite3.connect('my_database.db')
+##cursor = connection.cursor()
+##cursor.execute('SELECT SUM(age) FROM Users')
+##total_age = cursor.fetchone()[0]
+##
+##print('Общая сумму возраство пользователей', total_age)
+##connection.close()
+
+
+##import sqlite3
+##connection = sqlite3.connect('my_database.db')
+##cursor = connection.cursor()
+##cursor.execute('SELECT MIN(age) FROM Users')
+##min_age = cursor.fetchone()[0]
+##
+##print('Минимальныи возраст среди пользователе', min_age)
+##connection.close()
+
+
 import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
-cursor.execute('''
-SELECT username, age, AVG(age)
-FROM Users
-GROUP BY age
-HAVING AVG(age) > ?
-ORDER BY age DESC
-''', (30,))
-results = cursor.fetchall()
-for row in results:
-    print(row)
+cursor.execute('SELECT MAX(age) FROM Users')
+max_age = cursor.fetchone()[0]
 
+print('Max возраст среди пользователе', max_age)
 connection.close()

0edad695bf2c9c2387bcecdfb594022905940b32
ShireNine <168739802+ShireNine@users.noreply.github.com>
2025-02-19 19:55:22 +0500

    6_File

--- Prakitka.py
+++ Prakitka.py
@@ -1,10 +1,27 @@
+##import sqlite3
+##connection = sqlite3.connect('my_database.db')
+##cursor = connection.cursor()
+##
+##try:
+##    cursor.execute('BEGIN')
+##    cursor.execute('INSERT INTO Users(username, email) VALUES (?, ?)', ('user2', 'ewrwrew@fdfd'))
+##
+##    cursor.execute('COMMIT')
+##
+##except:
+##    cursor.execute('ROLLBACK')
+##
+##connection.close()
+
+
 import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
-cursor.execute('SELECT * FROM Users WHERE age IS NULL')
-unknow = cursor.fetchall()
 
-for user in unknow:
-    print(user)
-
-connection.close()
+try:
+    with connection:
+        cursor.execute('INSERT INTO Users(username, email) VALUES (?, ?)', ('users3', 'fsdsf@mail0110'))
+        cursor.execute('INSERT INTO Users(username, email) VALUES (?, ?)', ('users4', 'fsdsf@mail00'))
+except:
+    pass
+        

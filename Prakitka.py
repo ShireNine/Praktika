1380f1141604b3dbffd1216c7ca4b970133f6c9a
ShireNine <168739802+ShireNine@users.noreply.github.com>
2025-02-19 18:53:46 +0500

    4A_File

--- Prakitka.py
+++ Prakitka.py
@@ -1,9 +1,36 @@
+##import sqlite3
+##connection = sqlite3.connect('my_database.db')
+##cursor = connection.cursor()
+##cursor.execute('SELECT username, age FROM Users WHERE age > ?', (25,))
+##result = cursor.fetchall()
+##
+##for row in result:
+##    print(row)
+##connection.close()
+
+##
+##import sqlite3
+##connection = sqlite3.connect('my_database.db')
+##cursor = connection.cursor()
+##cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age')
+##results = cursor.fetchall()
+##
+##for row in results:
+##    print(row)
+##
+##cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age HAVING AVG(age) > ?' (30,))
+##filtered_results = cursor.fetchall()
+##for row in filtered_results:
+##    print(row)
+##connection.close()
+
 import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
-cursor.execute('SELECT * FROM Users')
-users = cursor.fetchall()
-for user in users:
-    print(user)
+cursor.execute('SELECT username, age FROM Users ORDER BY age DESC')
+results = cursor.fetchall()
+
+for row in results:
+    print(row)
 
 connection.close()

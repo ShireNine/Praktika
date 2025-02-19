2523890ad6df22a24e58d991cc187e0aa6ea0c5c
ShireNine <168739802+ShireNine@users.noreply.github.com>
2025-02-19 12:58:54 +0500

    3Файл

--- Prakitka.py
+++ Prakitka.py
@@ -1,8 +1,26 @@
-import sqlite3
+##import sqlite3
+##
+##connection = sqlite3.connect('my_database.db')
+##cursor = connection.cursor()
+##
+##cursor.execute ('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', ('newuser', 'nesfgefe@mail.ru', 28))
+##connection.commit()
+##connection.close()
 
+##import sqlite3
+##connection = sqlite3.connect('my_database.db')
+##cursor = connection.cursor()
+##cursor.execute('UPDATE Users SET age = ? WHERE username = ?', (29, 'newuser'))
+##
+##connection.commit()
+##connection.close()
+
+
+import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
+cursor.execute('DELETE FROM Users WHERE username = ?', ('newuser'))
 
-cursor.execute('CREATE INDEX idx_email ON Users (email)')
+
 connection.commit()
 connection.close()

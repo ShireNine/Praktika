c765c778105c9d91dadd73498e5d968275031a80
ShireNine <168739802+ShireNine@users.noreply.github.com>
2025-02-19 19:13:55 +0500

    4B_File

--- Prakitka.py
+++ Prakitka.py
@@ -1,35 +1,14 @@
-##import sqlite3
-##connection = sqlite3.connect('my_database.db')
-##cursor = connection.cursor()
-##cursor.execute('SELECT username, age FROM Users WHERE age > ?', (25,))
-##result = cursor.fetchall()
-##
-##for row in result:
-##    print(row)
-##connection.close()
-
-##
-##import sqlite3
-##connection = sqlite3.connect('my_database.db')
-##cursor = connection.cursor()
-##cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age')
-##results = cursor.fetchall()
-##
-##for row in results:
-##    print(row)
-##
-##cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age HAVING AVG(age) > ?' (30,))
-##filtered_results = cursor.fetchall()
-##for row in filtered_results:
-##    print(row)
-##connection.close()
-
 import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
-cursor.execute('SELECT username, age FROM Users ORDER BY age DESC')
+cursor.execute('''
+SELECT username, age, AVG(age)
+FROM Users
+GROUP BY age
+HAVING AVG(age) > ?
+ORDER BY age DESC
+''', (30,))
 results = cursor.fetchall()
-
 for row in results:
     print(row)
 

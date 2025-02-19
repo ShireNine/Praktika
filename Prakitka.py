4f579d5c725dca6dfc910c3b072e8c225ec22985
ShireNine <168739802+ShireNine@users.noreply.github.com>
2025-02-19 13:03:50 +0500

    4File

--- Prakitka.py
+++ Prakitka.py
@@ -1,26 +1,9 @@
-##import sqlite3
-##
-##connection = sqlite3.connect('my_database.db')
-##cursor = connection.cursor()
-##
-##cursor.execute ('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', ('newuser', 'nesfgefe@mail.ru', 28))
-##connection.commit()
-##connection.close()
-
-##import sqlite3
-##connection = sqlite3.connect('my_database.db')
-##cursor = connection.cursor()
-##cursor.execute('UPDATE Users SET age = ? WHERE username = ?', (29, 'newuser'))
-##
-##connection.commit()
-##connection.close()
-
-
 import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
-cursor.execute('DELETE FROM Users WHERE username = ?', ('newuser'))
+cursor.execute('SELECT * FROM Users')
+users = cursor.fetchall()
+for user in users:
+    print(user)
 
-
-connection.commit()
 connection.close()

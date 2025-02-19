b3b3de42c842353e643e552203b6b7bbfdf4c9cf
ShireNine <168739802+ShireNine@users.noreply.github.com>
2025-02-19 19:46:09 +0500

    5B_File

--- Prakitka.py
+++ Prakitka.py
@@ -2,11 +2,19 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 cursor.execute('SELECT * FROM Users')
-one = cursor.fetchone()
-print(one)
+users = cursor.fetchall()
 
-first = cursor.fetchmany(5)
-print(first)
+users_list = []
+for user in users:
+    user_dict = {
+        'id': user[0],
+        'username': user[1],
+        'email': user[2],
+        'age': user[3],
+        }
+users_list.append(user_dict)
 
-alld = cursor.fetchall()
-print(alld)
+for user in users_list:
+    print(user)
+
+connection.close()

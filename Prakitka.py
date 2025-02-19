2e9f83a5aaac04ba5cb9af5fd178e07ebe7360b7
ShireNine <168739802+ShireNine@users.noreply.github.com>
2025-02-19 12:34:55 +0500

    Второй файл

--- Prakitka.py
+++ Prakitka.py
@@ -1,5 +1,17 @@
 import sqlite3
 
-conncetion = sqlite3.connect('my_database.db')
-conncetion.close()
-print(2)
+connection = sqlite3.connect('my_database.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users (
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER
+)
+''')
+
+print(cursor)
+connection.commit()
+connection.close()

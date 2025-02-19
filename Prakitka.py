942fd9748c0be88e5230d64bb7d65ec7029a3784
ShireNine <168739802+ShireNine@users.noreply.github.com>
2025-02-19 19:48:16 +0500

    5C_File

--- Prakitka.py
+++ Prakitka.py
@@ -1,20 +1,10 @@
 import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
-cursor.execute('SELECT * FROM Users')
-users = cursor.fetchall()
+cursor.execute('SELECT * FROM Users WHERE age IS NULL')
+unknow = cursor.fetchall()
 
-users_list = []
-for user in users:
-    user_dict = {
-        'id': user[0],
-        'username': user[1],
-        'email': user[2],
-        'age': user[3],
-        }
-users_list.append(user_dict)
-
-for user in users_list:
+for user in unknow:
     print(user)
 
 connection.close()
